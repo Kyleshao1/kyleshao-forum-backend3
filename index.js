b15778a7d0bd4c47a6f4b72943c4fe33ce365ed5
Kyleshao1 <Kyleshao@sina.com>
2025-11-11 18:38:20 +0800

    fix problems v1
    5

--- index.js
+++ index.js
@@ -212,4 +212,4 @@ app.get('/api/admin/run-weekly-decay', async (req,res) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, ()=> console.log('Backend listening on', PORT, 'backend_url=', BACKEND_URL));
+app.listen(PORT, '0.0.0.0',()=> console.log('Backend listening on', PORT, 'backend_url=', BACKEND_URL));
