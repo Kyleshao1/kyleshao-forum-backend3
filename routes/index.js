11ff474ae8491549b7a7e8d849e2495e9bd5454c
Kyleshao1 <Kyleshao@sina.com>
2025-11-11 18:19:13 +0800

    fix syntax error
    the fifth time

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const router = express.Router();
+
+router.use("/auth", authRouter);
+router.use("/posts", postsRouter);
+router.use("/profile", profileRouter);
+
+router.get("/", (req, res) => {
+  res.json({ message: "Forum backend router working!" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
