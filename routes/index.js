341d268efe2f21187914ceb2bde03898a13e8cd9
Kyleshao1 <Kyleshao@sina.com>
2025-11-11 18:17:18 +0800

    fix syntax error
    the fourth time

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// 把所有以 /api 开头的请求交给 router
-app.use("/api", router);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
