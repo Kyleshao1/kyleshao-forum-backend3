4de58bc0e865a1a2a293ddc0bc8efd2264e1b324
Kyleshao1 <Kyleshao@sina.com>
2025-11-11 18:15:39 +0800

    fix syntax error
    the third time

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import router from "./index.js";
 
 const app = express();
 app.use(cors());
@@ -11,3 +10,4 @@ app.use("/api", router);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+export default router;
